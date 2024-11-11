f49a384638afa75794d824c6e1cab1f798de8fed
Holmbergfan <109139074+Holmbergfan@users.noreply.github.com>
2024-11-11 02:50:31 +0100

    Delete .history directory

--- .history/src/routes/deaths_20241029141538.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const deathController = require('../controllers/deathController'); // Adjust the path as necessary
-
-// Define your routes here
-router.get('/', deathController.getAllDeaths);
-router.post('/', deathController.createDeath);
-
-module.exports = router;
